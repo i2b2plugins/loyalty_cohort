f499210b0dd000bef9079fa60b62cf86b91601cc
Darren W Henderson <darren.henderson@uky.edu>
2021-09-08 20:46:39 -0400

    Revert some prebuild steps  - scrape base tables again
    This commit reverts the obs_fact and vis_dim prebuild tables. They  are not performant at larger sites. It attempts a new way to build the #INCLPAT table that might be more consistent across sites.

--- ExtractFullReport.sql
+++ ExtractFullReport.sql
@@ -1,12 +1,17 @@
+/* optional truncate 
+TRUNCATE TABLE [dbo].[loyalty_dev_summary] 
+
+*/
+
 /* Alter @site parameter to your site */
-EXEC [dbo].[usp_LoyaltyCohort_opt] @indexDate = '20120201', @site='UKY', @lookbackYears=1, @gendered=0, @output=0
-EXEC [dbo].[usp_LoyaltyCohort_opt] @indexDate = '20120201', @site='UKY', @lookbackYears=1, @gendered=1, @output=0
-EXEC [dbo].[usp_LoyaltyCohort_opt] @indexDate = '20120201', @site='UKY', @lookbackYears=2, @gendered=0, @output=0
-EXEC [dbo].[usp_LoyaltyCohort_opt] @indexDate = '20120201', @site='UKY', @lookbackYears=2, @gendered=1, @output=0
-EXEC [dbo].[usp_LoyaltyCohort_opt] @indexDate = '20120201', @site='UKY', @lookbackYears=5, @gendered=0, @output=0
-EXEC [dbo].[usp_LoyaltyCohort_opt] @indexDate = '20120201', @site='UKY', @lookbackYears=5, @gendered=1, @output=0
-EXEC [dbo].[usp_LoyaltyCohort_opt] @indexDate = '20120201', @site='UKY', @lookbackYears=10, @gendered=0, @output=0
-EXEC [dbo].[usp_LoyaltyCohort_opt] @indexDate = '20120201', @site='UKY', @lookbackYears=10, @gendered=1, @output=0
+EXEC [dbo].[usp_LoyaltyCohort_opt] @indexDate = '20210201', @site='UKY', @lookbackYears=1, @gendered=0, @output=0
+EXEC [dbo].[usp_LoyaltyCohort_opt] @indexDate = '20210201', @site='UKY', @lookbackYears=1, @gendered=1, @output=0
+EXEC [dbo].[usp_LoyaltyCohort_opt] @indexDate = '20210201', @site='UKY', @lookbackYears=2, @gendered=0, @output=0
+EXEC [dbo].[usp_LoyaltyCohort_opt] @indexDate = '20210201', @site='UKY', @lookbackYears=2, @gendered=1, @output=0
+EXEC [dbo].[usp_LoyaltyCohort_opt] @indexDate = '20210201', @site='UKY', @lookbackYears=5, @gendered=0, @output=0
+EXEC [dbo].[usp_LoyaltyCohort_opt] @indexDate = '20210201', @site='UKY', @lookbackYears=5, @gendered=1, @output=0
+EXEC [dbo].[usp_LoyaltyCohort_opt] @indexDate = '20210201', @site='UKY', @lookbackYears=10, @gendered=0, @output=0
+EXEC [dbo].[usp_LoyaltyCohort_opt] @indexDate = '20210201', @site='UKY', @lookbackYears=10, @gendered=1, @output=0
 
 SELECT [SITE], [EXTRACT_DTTM], [LOOKBACK_YR], GENDER_DENOMINATORS_YN, [CUTOFF_FILTER_YN], [Summary_Description], [tablename], [Num_DX1], [Num_DX2], [MedUse1], [MedUse2]
 , [Mammography], [PapTest], [PSATest], [Colonoscopy], [FecalOccultTest], [FluShot], [PneumococcalVaccine], [BMI], [A1C], [MedicalExam], [INP1_OPT1_Visit], [OPT2_Visit], [ED_Visit]
@@ -17,6 +22,6 @@ SELECT [SITE], [EXTRACT_DTTM], [LOOKBACK_YR], GENDER_DENOMINATORS_YN, [CUTOFF_FI
 , FORMAT(1.0*[TotalSubjectsMale]/[TotalSubjects],'P') AS PercentMale
 , [RUNTIMEms]
 FROM [dbo].[loyalty_dev_summary] 
-WHERE Summary_Description = 'PercentOfSubjects' 
+WHERE Summary_Description = 'PercentOfSubjects' and convert(date,EXTRACT_DTTM) = convert(date,getdate())
 ORDER BY GENDER_DENOMINATORS_YN, LOOKBACK_YR, CUTOFF_FILTER_YN, TABLENAME;
 

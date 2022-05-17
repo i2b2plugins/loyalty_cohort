d17a792fc8083f1104973392191c18ce27cfb95d
mim18 <mim18@pitt.edu>
2022-05-17 12:27:03 -0400

    Views to turn OMOP tables into multifact i2b2 tables

---
+++ OMOP-Helper-Files/ALL_SOURCE_CONCEPTS_COND.sql
@@ -0,0 +1,54 @@
+--------------------------------------------------------
+--  File created - Thursday-January-13-2022   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for View ALL_SOURCE_CONCEPTS_COND
+--------------------------------------------------------
+
+  CREATE OR REPLACE FORCE EDITIONABLE VIEW "NCATSTEST_CRCDATA"."ALL_SOURCE_CONCEPTS_COND" ("ENCOUNTER_NUM", "PATIENT_NUM", "CONCEPT_CD", "PROVIDER_ID", "START_DATE", "END_DATE", "MODIFIER_CD", "INSTANCE_NUM", "VALTYPE_CD", "LOCATION_CD", "TVAL_CHAR", "NVAL_NUM", "VALUEFLAG_CD", "UNITS_CD", "STANDARD_CONCEPT_ID", "SOURCE_VALUE", "DOMAIN_ID") AS 
+  SELECT
+			ENCOUNTER_NUM, 
+ 			PATIENT_NUM, 
+   			CONCEPT_CD, 
+   			PROVIDER_ID, 
+   			START_DATE, 
+			END_DATE, 
+            MODIFIER_CD,
+            INSTANCE_NUM,
+            valtype_cd,
+            location_cd,
+            tval_char,
+            nval_num,
+            valueflag_cd,
+            units_cd,
+            STANDARD_CONCEPT_ID,
+            SOURCE_VALUE,
+            DOMAIN_ID
+FROM (
+ SELECT  
+			visit_occurrence_id AS ENCOUNTER_NUM, 
+ 			PERSON_ID AS PATIENT_NUM, 
+   			cast(condition_source_concept_id as varchar(50)) AS CONCEPT_CD, 
+   			CAST(provider_id AS VARCHAR(50)) AS PROVIDER_ID, 
+   			condition_start_datetime AS START_DATE, 
+			condition_end_datetime AS END_DATE, 
+            NULL AS MODIFIER_CD,
+            NULL AS INSTANCE_NUM,
+            NULL AS valtype_cd,
+            NULL AS location_cd,
+            NULL AS tval_char,
+            NULL AS nval_num,
+            NULL AS valueflag_cd,
+            NULL AS units_cd,
+   			condition_concept_id AS STANDARD_CONCEPT_ID,
+            condition_source_value AS SOURCE_VALUE,
+            'CONDITION' AS DOMAIN_ID
+--			condition_occurrence_id integer NOT NULL, 
+--			condition_start_datetime TIMESTAMP NULL, 
+--			condition_end_datetime TIMESTAMP NULL, 
+--			condition_type_concept_id integer NOT NULL, --modifier?
+--			stop_reason varchar(20) NULL, 
+--			visit_detail_id integer NULL, 
+--			condition_status_source_value varchar(50) NULL );  
+FROM CONDITION_OCCURRENCE)
+;
